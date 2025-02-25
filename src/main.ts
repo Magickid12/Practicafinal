0a866c1c634511b85e0f3bc866741a8d8fe540db
Magickid12 <sevsas02@gmail.com>
2025-02-25 15:44:47 -0600

    Actualización de archivos de la API

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,14 @@ async function bootstrap() {
     }),
   );
 
+  // *** AÑADE ESTO ***
+  app.enableCors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:3001'], // O '*' para permitir todos los orígenes (menos seguro)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Opcional: Especifica los métodos permitidos
+    allowedHeaders: ['Content-Type'], // Opcional: Especifica las cabeceras permitidas
+  });
+  // *** FIN DE LO QUE AÑADISTE ***
+
   await app.listen(3000);
   console.log(`App running at http://localhost:3000`);
 }
